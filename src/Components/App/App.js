27c6f251d3ad4f0ea8de0fdcf4d80e02f6b9744e
AndreBaixista1 <andrbter@gmail.com>
2022-05-19 11:55:36 -0300

    Curso concluido

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,10 @@
-import SmartCounter from '../SmartCounter/SmartCounter'
-
+import IfoodCounter from '../IfoodCounter/IfoodCounter.js';
 import './App.css';
 
 function App() {
   return (<>
-    <h1>Hello World!</h1>
-    <SmartCounter/>
+    <h1>iFood</h1>
+    <IfoodCounter/>
     </>
   );
 }
